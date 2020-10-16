a4fb3ab2c1b4560e40078bc71ee7a9eee7fdb542
Darrence Bernadas <69776186+pipikapi@users.noreply.github.com>
2020-10-16 22:31:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const db = mongoose.connection
 db.on('error',(error)=>console.error(error))
 db.once('open',()=>console.log('Connected to Database'))
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 const routes = require('../API/routes/router');
 routes(server);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log('Server Started'))
+server.listen(PORT, ()=> console.log('Server Started'))
